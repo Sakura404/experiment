d8c0072018f76c215eb56fcd128dc3309b6e3ecc
Sakura404 <380487608@qq.com>
2021-04-06 19:01:57 +0800

    q

---
+++ java web/first/chapter03/src/cn/itcast/servlet/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloWorldServlet extends GenericServlet {
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("hello");
+	}
+
+	public void service (ServletRequest request, ServletResponse respones) throws ServletException {
+		System.out.println("them");
+	}
+
+	public void destroy() {
+		System.out.println("miss me?");
+	}
+
+}
---
+++ java web/first/chapter03/src/cn/itcast/servlet/Servlet01.java
@@ -0,0 +1,24 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet01
+ */
+@WebServlet("/Servlet01")
+public class Servlet01 extends GenericServlet {
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("this is power");
+	}
+}
